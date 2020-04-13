0f1b8dbf6b51814ef48f6e4f7e1059c30dd6524c
Grant Yoshitsu <grantkoji@gmail.com>
2020-04-13 18:34:46 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,1 +1,10 @@
 # Write your code here
+magic_exit_number = 0
+count = 10
+while count >= 0 do
+  break if count == magic_exit_number
+    puts "Happy New Year!"
+  else
+  puts count
+  count -= 1
+end  
