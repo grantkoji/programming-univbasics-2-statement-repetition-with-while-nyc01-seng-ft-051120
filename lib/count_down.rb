e70fdead3e6636656cb8c706494aed491908576b
Grant Yoshitsu <grantkoji@gmail.com>
2020-04-13 18:37:36 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,10 +1,7 @@
 # Write your code here
-magic_exit_number = 0
 count = 10
-while count >= 0 do
-  break if count == magic_exit_number
-    puts "Happy New Year!"
-  else
-  puts `${count}`
+while count >= 1 do
+  puts count
   count -= 1
 end
+puts "Happy New Year!"
